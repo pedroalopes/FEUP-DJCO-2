85086c88336dce541f37ae9ee0cf46ea3b4fcb9a
Antonio Cruz <acruz9823@gmail.com>
2020-04-19 19:26:07 +0100

    Reworked cube pick up

--- New Unity Project/Assets/Scripts/PickUp.cs
+++ New Unity Project/Assets/Scripts/PickUp.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    public Transform destination;
-    private bool pickedUp = false;
+    Vector3 objectPos;
+    float distance;
 
-    public void Interact()
-    {
-        if (!pickedUp)
-        {
-            GetComponent<Rigidbody>().useGravity = false;
-            GetComponent<Rigidbody>().freezeRotation = true;
-        } else
-        {
-            GetComponent<Rigidbody>().useGravity = true;
-            GetComponent<Rigidbody>().freezeRotation = false;
-        }
-        pickedUp = !pickedUp;
-
-
-    }
+    public bool canHold = true;
+    public GameObject item;
+    public GameObject tempParent;
+    public bool isHolding = false;
+    public float pickUpDistance = 1f;
 
     private void Update()
     {
-        if (pickedUp)
+        distance = Vector3.Distance(item.transform.position, tempParent.transform.position);
+        if(distance > pickUpDistance)
         {
-            this.transform.position = destination.position;
+            isHolding = false;
         }
+
+        if (isHolding)
+        {
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.SetParent(tempParent.transform);
+        } else
+        {
+            objectPos = item.transform.position;
+            item.transform.SetParent(null);
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.transform.position = objectPos;
+        }
+    }
+
+    public void Interact()
+    {
+        if (!isHolding)
+        {
+            if (distance <= pickUpDistance)
+            {
+                item.GetComponent<Rigidbody>().useGravity = false;
+                item.GetComponent<Rigidbody>().detectCollisions = true;
+            }
+            else return;
+        }
+
+        isHolding = !isHolding;
     }
 }
