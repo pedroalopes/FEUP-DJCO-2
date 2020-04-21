9ca085c064962a2a56fb19754ce77bdb067d6f67
FabioMNAraujo <FabioAraujo@Outlook.pt>
2020-04-21 21:28:03 +0100

    add damage and force and livingTime to fireObjects

--- New Unity Project/Assets/Scripts/FireObjectScript.cs
+++ New Unity Project/Assets/Scripts/FireObjectScript.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class FireObjectScript : MonoBehaviour
 {
+    private const float livingTime = 10.0f;
+    private double damage;
+    private int force;
+    private float startTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        damage = 1;
+        force = 600;
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time >= startTime + livingTime) {
+            printStatus();
+
+            Destroy(gameObject);
+        }
+    }
+
+    public void addDamage() {
+        damage += 0.1;
+    }
+
+    public void removeForce() {
+        force -= 6;
     }
     
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision");
+        printStatus();
+
         Destroy(gameObject);
+    }
+
+    public double getForce() {
+        return this.force;
+    }
+
+    public void printStatus() {
+        Debug.Log("Collision");
+        Debug.Log("Damage: " + damage);
+        Debug.Log("Force: " + force);
     }
 }
