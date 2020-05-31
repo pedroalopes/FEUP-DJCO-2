647c89fbfac4d463aac68e05c7cb2a957ef99c15
Antonio Cruz <acruz9823@gmail.com>
2020-05-31 22:33:59 +0100

    Added Temp Char to scene

---
+++ New Unity Project/Assets/Scripts/BraceletColour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BraceletColour : MonoBehaviour
+{
+    public Material[] materials = new Material[4];
+    public PlayerController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (controller.element)
+        {
+            case Element.Earth:
+                gameObject.GetComponent<Renderer>().material = materials[0];
+                break;
+            case Element.Water:
+                gameObject.GetComponent<Renderer>().material = materials[1];
+                break;
+            case Element.Wind:
+                gameObject.GetComponent<Renderer>().material = materials[2];
+                break;
+            case Element.Fire:
+                gameObject.GetComponent<Renderer>().material = materials[3];
+                break;
+
+        }
+    }
+}
