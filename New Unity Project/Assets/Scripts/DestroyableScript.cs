e5a3c56c6d21ebdec2b339e5b16f094b52c8ef2a
sendim <henrique000@live.com.pt>
2020-06-11 03:32:12 +0100

    Add jump land and ambience sounds

--- New Unity Project/Assets/Scripts/DestroyableScript.cs
+++ New Unity Project/Assets/Scripts/DestroyableScript.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class DestroyableScript : MonoBehaviour
 {
+    [FMODUnity.EventRef]
+    private string DestroyEvent = "event:/main SFX/SFX #1/fire/destroy_stones_export";
+    FMOD.Studio.EventInstance destroy;
+	
     public float racio = 1.5f;
     private float occultTime = 0.0f;
     public GameObject[] childs = new GameObject[6];
     // Start is called before the first frame update
     void Start()
     {
+		destroy = FMODUnity.RuntimeManager.CreateInstance(DestroyEvent);
     }
 
     // Update is called once per frame
     void Update()
     {
+		destroy.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
+		
         if (occultTime > 0)
         {
+			
             for (int i = 0; i < 6; i++)
             {
                 childs[i].GetComponent<MeshRenderer>().enabled = false;
@@ -33,6 +41,7 @@ public class DestroyableScript : MonoBehaviour
     public void occultObject(float damage)
     {
         this.occultTime = racio * damage;
+		destroy.start();
 
     }
 
--- New Unity Project/Assets/Scripts/Elements/FireController.cs
+++ New Unity Project/Assets/Scripts/Elements/FireController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FireController : MonoBehaviour
 {
+
+    [FMODUnity.EventRef]
+    public string FireShootEvent = "";
+    FMOD.Studio.EventInstance fireShoot;
+	
     public Transform firePrefab;
     public GameObject firePoint;
     private Transform fireObject;
@@ -12,12 +17,14 @@ public class FireController : MonoBehaviour
     {
         if (fireObject == null)
         {
+			fireShoot = FMODUnity.RuntimeManager.CreateInstance(FireShootEvent);
             fireObject = Instantiate(firePrefab, Camera.main.transform.position + (Camera.main.transform.forward * 1), transform.rotation);
             fireObject.SetParent(firePoint.transform);
         }
     }
     public void handleCharge()
     {
+        fireShoot.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
         if (fireObject == null)
             return;
 
@@ -41,5 +48,6 @@ public class FireController : MonoBehaviour
         rb.AddForce(shoot * (int)fireObject.GetComponent<FireObjectScript>().getForce());
         fireObject.SetParent(null);
         fireObject = null;
+		fireShoot.start();
     }
 }
