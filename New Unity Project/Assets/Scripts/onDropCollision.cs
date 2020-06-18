d3431007b4b3965185c94473ce6f8ccfb4639637
Pedro <lopes.pedroa@gmail.com>
2020-06-18 01:50:28 +0100

    Added dripping water to pipe in all scenes

---
+++ New Unity Project/Assets/Scripts/onDropCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onDropCollision : MonoBehaviour
+{
+    public void SpawnObject()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Vector3.zero;
+        rb.velocity = Vector3.zero;
+
+        StartCoroutine(DestroyObject());
+    }
+
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(0.9f);
+        gameObject.SetActive(false);
+    }
+}
